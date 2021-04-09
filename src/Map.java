e8758229c87e47787dd03a03ae0fed64d37caa29
Matthew Oros <orosmatthew@gmail.com>
2021-04-08 20:39:51 -0400

    added smooth map movement

--- src/Map.java
+++ src/Map.java
@@ -1,25 +1,35 @@
 import Framework.Engine;
 import Framework.Object.CanvasObject;
+import Framework.Type.Vector2;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
 public class Map extends CanvasObject {
 
-    BufferedImage image;
-    TileServer tileServer;
-    String MapboxAccessToken = "pk.eyJ1Ijoib3Jvc21hdHRoZXciLCJhIjoiY2ttczM4MGxxMGR0YjJ2bnhqa2ZpcnF3diJ9.WBoUUsXLofAZC9xM52N-oQ";
-    int tileX;
-    int tileY;
-    int zoom;
+    private final BufferedImage[] images = new BufferedImage[9];
+    private final Vector2[] imagePositions = {new Vector2(-1, 1), new Vector2(0, 1), new Vector2(1, 1),
+        new Vector2(-1, 0), new Vector2(0, 0), new Vector2(1, 0),
+        new Vector2(-1, -1), new Vector2(0, -1), new Vector2(1, -1)};
+    private TileServer tileServer;
+    private String MapboxAccessToken = "pk.eyJ1Ijoib3Jvc21hdHRoZXciLCJhIjoiY2ttczM4MGxxMGR0YjJ2bnhqa2ZpcnF3diJ9.WBoUUsXLofAZC9xM52N-oQ";
+    private int tileX;
+    private int tileY;
+    private int zoom;
+    private float speed = 10;
+
+    Vector2 position;
 
     @Override
     public void init() {
 
         tileServer = new CachedTileServer();
 
-        zoom = 10;
+        position = new Vector2();
+
+        zoom = 7;
         double lat = 41.347881d;
         double lon = -81.808503d;
 
@@ -32,33 +42,55 @@ public class Map extends CanvasObject {
     }
 
     private void displayTile() {
-        TileServerRequest request = new MapboxRequest(tileX, tileY, zoom, MapboxAccessToken, true);
-        image = tileServer.getTile(request);
+        for (int i = 0; i < 9; i++) {
+            TileServerRequest request = new MapboxRequest(tileX + (int)imagePositions[i].x, tileY + (int)imagePositions[i].y, zoom, MapboxAccessToken, true);
+            images[i] = tileServer.getTile(request);
+        }
     }
 
     @Override
     public void process(double delta) {
-        if (Engine.getInput().isKeyJustPressed(KeyEvent.VK_UP)) {
-            tileY--;
-            displayTile();
+        if (Engine.getInput().isKeyPressed(KeyEvent.VK_W)) {
+            position.y += speed;
         }
-        if (Engine.getInput().isKeyJustPressed(KeyEvent.VK_DOWN)) {
-            tileY++;
-            displayTile();
+        if (Engine.getInput().isKeyPressed(KeyEvent.VK_S)) {
+            position.y -= speed;
         }
-        if (Engine.getInput().isKeyJustPressed(KeyEvent.VK_LEFT)) {
+        if (Engine.getInput().isKeyPressed(KeyEvent.VK_A)) {
+            position.x += speed;
+        }
+        if (Engine.getInput().isKeyPressed(KeyEvent.VK_D)) {
+            position.x -= speed;
+        }
+
+        if (position.x > 500) {
+            position.x = 0;
             tileX--;
             displayTile();
         }
-        if (Engine.getInput().isKeyJustPressed(KeyEvent.VK_RIGHT)) {
+        if (position.x < -500) {
+            position.x = 0;
             tileX++;
             displayTile();
         }
+        if (position.y > 500) {
+            position.y = 0;
+            tileY--;
+            displayTile();
+        }
+        if (position.y < -500) {
+            position.y = 0;
+            tileY++;
+            displayTile();
+        }
+
     }
 
     @Override
     public void drawObject(Graphics g) {
-        g.drawImage(image, 0, 0, 500, 500, null);
+        for (int i = 0; i < 9; i++) {
+            g.drawImage(images[i], (int)(position.x + (500 * imagePositions[i].x)), (int)(position.y + (500 * imagePositions[i].y)), 500, 500, null);
+        }
     }
 
 
